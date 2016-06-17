370f83ac458d1d076896e0e056c518f1bc7bbf97
v5zhu@qq.com <longzhu20*>
2016-06-17 10:33:26 +0800

    增加logback日志打印，打印信息包含：当前日期、线程名称、日志级别、类全路径、行号

--- src/main/java/com/v5zhu/open/quartz/MyJob.java
+++ src/main/java/com/v5zhu/open/quartz/MyJob.java
@@ -1,12 +1,16 @@
 package com.v5zhu.open.quartz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Date;
 
 /**
  * Created by zhuxl@paxsz.com on 2016/6/16.
  */
 public class MyJob {
+    private static final Logger logger= LoggerFactory.getLogger(MyJob.class);
     public void execute(){
-        System.out.println("当前时间:"+new Date().toString());
+        logger.info("当前时间：[{}]",new Date().toString());
     }
 }
